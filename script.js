83818daaef007cb011a63e0245edca22e14d8470
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-19 18:00:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ function calculateMinCost() {
   // read data from input element;
   var data = document.querySelector("#rope-lengths").value;
   var arr = data.split(",");
-
-  // for (var i = 0; i < arr.length; i++) {
-  //   arr[i] = Number(arr[i]);
-  // }
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = Number(arr[i]);
+  }
   var cost = 0;
 	arr.sort(function(a,b){return a-b});
-	
-
-  while (arr.length > 1) {
+	while (arr.length > 1) {
     var newRope = arr[0] + arr[1];
     cost += newRope;
 
